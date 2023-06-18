4cd9adc8dd9403e503cd36a5da6c9b22d7cd9612
UnesseAh <unesseah56@gmail.com>
2023-06-18 11:08:23 -0700

    print alphabet from a to z

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description : "Print alphabet in lowercase, followed by a new line."
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n = 'a';
+	while(n <= 'z')
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
